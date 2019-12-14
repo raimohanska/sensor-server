ae45ad68172bf6965a5e2759cf2793c7f43a911c
Juha Paananen <juha.paananen@gmail.com>
2019-12-14 15:52:15 +0200

    use npm ping module

--- src/pinger.js
+++ src/pinger.js
@@ -1,30 +1,35 @@
 const B=require("baconjs");
 let log=require("./log");
+const ping = require("ping");
 const {
   exec
 } = require('child_process');
 
+function silence(duration) {
+  return B.later(duration).filter(false)
+}
+
 const pingE = function(hostname, options) {
   if (options == null) { options = {}; }
   const interval = options.interval || 1000;
-  const ping = function() {
-    const ps = exec("ping -c 1 " + hostname + " -W 1");
-    return B.fromEvent(ps, "exit").take(1).delay(interval);
+  const mkPing = function() {
+    return B.fromPromise(ping.promise.probe(hostname)).concat(silence(interval))
   };
-  log = function() { return console.log.apply(console, [new Date()].concat(Array.prototype.slice.call(arguments))); };
-  return B.repeat(ping).map(exitCode => exitCode === 0);
+  return B.repeat(mkPing)
 };
 
 const hostDownE = function(hostname, options) {
   if (options == null) { options = {}; }
   const downCount = options.downCount || 10;
-  const ping = pingE(hostname, options);
+  const ping = pingE(hostname, options).map(p => p.alive)
   const pingSuccessE = ping.filter(x => x);
   const pingFailE = ping.filter(x => !x);
-  const tenConsecutiveFails = () => pingSuccessE.take(1).filter(false) // wait for first success
+  const tenConsecutiveFails = () => pingSuccessE.take(1).doAction(log, "ping ok: " + hostname).filter(false) // wait for first success
   .concat(pingFailE.take(1).filter(false)) // wait for first failure
-  .concat(pingFailE.take(downCount - 1).doAction(log, "ping fail: " + hostname).last().takeUntil(pingSuccessE.doAction(log, "ping ok: " + hostname)));
+  .concat(pingFailE.take(downCount - 1).doAction(log, "ping fail: " + hostname).last().map("Host down: " + hostname).takeUntil(pingSuccessE));
   return B.repeat(tenConsecutiveFails);
 };
+
+//hostDownE("192.168.1.1", { downCount: 2 } ).log()
  
 module.exports = { hostDownE, pingE };
